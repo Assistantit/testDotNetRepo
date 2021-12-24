304008fa0d9afa6ec2185ac3c1f54839f940d74f
Maksym Zhylin <makz@sitecore.com>
2021-12-24 09:57:00 +0200

    add C# test prj

---
+++ CSharpCource/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CSharpCource
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static void MyFunction()
+            {
+                Console.WriteLine("Hello World!");
+                string myString = "my friend!";
+                Console.WriteLine(myString);
+                int x = 1;
+                int j = x++;
+                Console.WriteLine(j + " and " + x);
+                x--;
+                j = ++x;
+                Console.WriteLine(j + " and " + x);
+                Console.Read();
+            }
+            
+        }
+        
+    }
+}
